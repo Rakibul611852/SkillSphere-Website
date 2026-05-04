82ee699d2e554a8b68223a2ef7b87955855cd3b4
Rakibul611852 <rakibulislam611852@gmail.com>
2026-05-04 09:46:03 +0600

    added proxy.js to protect routes

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -6,7 +6,7 @@ import React from 'react'
 
 const page = () => {
     const userData = authClient.useSession()
-     const user = userData.data?.user
+     const user = userData.data?.user;
        
     
 
